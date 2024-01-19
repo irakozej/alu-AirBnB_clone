bffed7743a2c3b542c291e742b60e34b703b23f1
Mugara374 <m.mushi@alustudent.com>
2024-01-19 18:01:42 +0200

    console

---
+++ console.py
@@ -0,0 +1,29 @@
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+
+    """
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """
+
+        """
+        return True
+    
+    def help_quit(self, arg):
+        """
+
+        """
+        print("quit command to exit the program")
+
+    def do_EOF(self, arg):
+        """
+
+        """
+        print()
+        return True
+    
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
